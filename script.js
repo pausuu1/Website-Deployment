b432e3a3d0310e77573c3cd8aab4ea4a9f8623b4
Pausuu YouTube <tbgsquid.real@gmail.com>
2026-05-06 00:24:15 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showSection(id) {
+    document.querySelectorAll('.section').forEach(sec => sec.classList.remove('active'));
+    document.getElementById(id).classList.add('active');
+    window.scrollTo(0, 0);
+}
